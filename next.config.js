1e375052bca5c24bf1b56b2a2b36bdacb8c5ea3e
kdeptula <contact@deptula.it>
2025-12-04 19:52:53 +0100

    fix wasm

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  webpack: (config, { isServer }) => {
+    // Copy web-ifc.wasm to the output directory where the library expects it
+    if (!isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              from: path.join(__dirname, 'node_modules/web-ifc-three/node_modules/web-ifc/web-ifc.wasm'),
+              to: path.join(__dirname, '.next/static/chunks/wasm/web-ifc.wasm'),
+            },
+          ],
+        })
+      );
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
